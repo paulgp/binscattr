39e68cfbffe431056ad287c6ff8f66500aeeba15
Paul Goldsmith-Pinkham <paulgp@gmail.com>
2017-11-07 09:47:18 -0500

    Updated ReadME with TODOs

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # binscattr
+
+* Todo:
+  1) Rotate postiions around graph
+  2) Move regressions inside discrete vs not, so it's not wastefully running regs
+  3) Figure out why the text looks so crappy
+  4) Connected line graphs
+  5) label graphs
--- package.R
+++ package.R
@@ -86,9 +86,3 @@ data <- airquality %>% na.omit
 binscatter(data, y="Ozone", x ="Temp", discrete=FALSE, controls=c("Solar.R", "Wind" ))
 binscatter(data, y="Ozone", x ="Temp", discrete=FALSE)
 
-## Todo:
-## 1) Rotate postiions around graph
-## 2) Move regressions inside discrete vs not, so it's not wastefully running regs
-## 3) Figure out why the text looks so crappy
-## 4) Connected line graphs
-## 5) label graphs
