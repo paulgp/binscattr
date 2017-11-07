867b12eed8504ffbc3fdcf4f93daae0d743898c2
Shoshana Vasserman <shoshievass@Shoshanas-MacBook-Pro.local>
2017-11-07 11:01:25 -0500

    Adding a library-ready binscattr folder/project

---
+++ binscattr/R/binscatter.R
@@ -0,0 +1,85 @@
+#' Bin-scatter function
+#'
+#' This function does a binscatter for two-dimensional data
+#' @param data Dataframe containing the x and y variables
+#' @param y The independent variable y
+#' @param x The dependent variable x
+#' @param bins The number of bins
+#' @param discrete Discrete?
+#' @param scatter Not sure what this is yet...
+#' @param theme Optional ggplot theme
+#' @param fitline Option to add a linear fit through the bin scatter
+#' @keywords binscatter
+#' @export
+#' @examples
+#' binscatter()
+
+binscatter <- function(data, y, x, bins=20, discrete=FALSE, scatter=FALSE,
+                       theme=theme_binscatter, fitline=TRUE, controls=c(), absorb=c("0"),
+                       clustervars=c("0"), pos="bottom right") {
+  library(ggplot2)
+  library(lfe)
+  library(dplyr)
+  library(broom)
+
+
+
+  if(length(controls) == 0) {
+    formula = as.formula(paste(y, "~", x  , "|" ,
+                               paste(absorb,sep="",collapse=" + ") , "|" , "0" ,  "|" ,
+                               paste(clustervars,sep="",collapse=" + "), sep=" "))
+  }
+  if(length(controls)!=0) {
+    formula = as.formula(paste(y, "~", x, "+", paste(controls,sep="",collapse=" + ") , "|" ,
+                               paste(absorb,sep="",collapse=" + ") , "|" , "0" ,  "|" ,
+                               paste(clustervars,sep="",collapse=" + "), sep=" "))
+
+    y_res_formula = as.formula(paste(y, "~", paste(controls,sep="",collapse=" + ") , "|" ,
+                                     paste(absorb,sep="",collapse=" + ") , "|" , "0" ,  "|" ,
+                                     paste(c("0"),sep="",collapse=" + "), sep=" "))
+    x_res_formula = as.formula(paste(x, "~", paste(controls,sep="",collapse=" + ") , "|" ,
+                                     paste(absorb,sep="",collapse=" + ") , "|" , "0" ,  "|" ,
+                                     paste(c("0"),sep="",collapse=" + "), sep=" "))
+    controls <- data[,controls]
+  }
+  x_label = x
+  y_label = y
+  x <- data[,x]
+  y <- data[,y]
+  f <- felm(formula, data=data)
+  print(tidy(f)[2,2:3])
+  beta <- paste("beta", formatC(tidy(f)[2,2], digits=3,format="fg", flag="#"), sep="=")
+  se <-   paste("s.e.", formatC(tidy(f)[2,3], digits=3,format="fg", flag="#"), sep="=")
+  if(length(controls) != 0) {
+    f_Xres <- felm(x_res_formula, data=data)
+    f_Yres <- felm(y_res_formula, data=data)
+    x <- f_Xres$residuals + mean(x)
+    y <- f_Yres$residuals + mean(y)
+  }
+  g <- ggplot(data, aes(x = x , y= y))  + theme() +
+    xlab(x_label) + ylab(y_label)
+  if (scatter == TRUE) {
+    g <- g + geom_point()
+  }
+  if (discrete == TRUE) {
+    g <- g + stat_summary(fun.y = "mean",  colour = "#0072B2", size = 2, geom="point")
+  }
+  else {
+    g <- g + stat_summary_bin(fun.y = "mean",  colour = "#0072B2", size = 2, geom="point", bins=20)
+  }
+  if (fitline == TRUE) {
+    g <- g + geom_smooth(method='lm',formula=y~x, se=FALSE, color="#D55E00", size=1)
+    posx <- c(Inf, Inf, -Inf, -Inf)
+    posy <- c(Inf, -Inf, Inf, -Inf)
+    posname <- c("top right", "bottom right", "top left", "bottom left")
+    adjh <- c(1,1,-1,-1)
+    adjv <- c(1,-1,1,-1)
+    posdf <- data.frame(posx, posy, adjh, adjv, row.names=posname)
+
+    print(posdf)
+    g <- g +
+      geom_text(aes(x=Inf, y=-Inf,hjust=1, vjust=-2.5, label=beta)) +
+      geom_text(aes(x=Inf, y=-Inf,hjust=1, vjust=-1, label=se))
+  }
+  return(g)
+}
